8ae08280fadcc94ae0e749207d6dc9851595ff6e
ahmednoorzakaria <ahmednoor.zakaria@student.moringaschool.com>
2023-06-27 19:47:54 +0300

    finished palindromes

--- index.js
+++ index.js
@@ -1,13 +1,26 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  const reversedWord = word.split('').reverse().join('');
+  if(word === reversedWord){
+    return true
+  }else {
+    return false
+  }
 }
+console.log(isPalindrome('mom'))
 
 /* 
   Add your pseudocode here
+  reverse the word
+  check if the reversed word is same as the word 
+  if it is same return true
+  if its false return false
 */
 
 /*
   Add written explanation of your solution here
+  when reversing the word you turn the word into 
+  an array first then reverse it the joint it
 */
 
 // You can run `node index.js` to view these console logs
@@ -15,11 +28,9 @@ if (require.main === module) {
   // add your own custom tests in here
   console.log("Expecting: true");
   console.log("=>", isPalindrome("racecar"));
-
   console.log("");
-
-  console.log("Expecting: false");
-  console.log("=>", isPalindrome("robot"));
+   console.log("Expecting: false");
+    console.log("=>", isPalindrome("robot"));
 }
 
 module.exports = isPalindrome;
